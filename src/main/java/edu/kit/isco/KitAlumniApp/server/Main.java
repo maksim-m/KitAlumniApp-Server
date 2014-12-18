3b244d18b9206d0b825937260883698f319a279b
Maksim Moiseikin <m.moiseikin@gmail.com>
2014-12-18 16:48:54 +0100

    Initial commit.

---
+++ src/main/java/edu/kit/isco/KitAlumniApp/server/Main.java
@@ -0,0 +1,10 @@
+package edu.kit.isco.KitAlumniApp.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
