ced1ec144a6fcc621212cc2acf819e894151c640
ioksrom <ioksrom@gmail.com>
2014-12-18 23:07:25 +0100

    Checkstyle config, Maven build settings.

--- src/main/java/edu/kit/isco/KitAlumniApp/server/Main.java
+++ src/main/java/edu/kit/isco/KitAlumniApp/server/Main.java
@@ -1,7 +1,18 @@
 package edu.kit.isco.KitAlumniApp.server;
 
-public class Main {
+/**
+ * @author Maksim Moiseikin
+ * @version 0.1
+ *
+ */
+public final class Main {
+	
+	private Main() { }
 
+	/**
+	 * Hauptmethode.
+	 * @param args Kommandzeilearguments
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
