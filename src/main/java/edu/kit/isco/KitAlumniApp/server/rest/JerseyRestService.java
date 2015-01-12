3e86a859b9c96bd206f63108e9e84f675a3db9e4
ioksrom <ioksrom@gmail.com>
2015-01-12 16:41:39 +0100

    Updated RestService and project settings.

--- src/main/java/edu/kit/isco/KitAlumniApp/server/rest/JerseyRestService.java
+++ src/main/java/edu/kit/isco/KitAlumniApp/server/rest/JerseyRestService.java
@@ -3,7 +3,9 @@ package edu.kit.isco.KitAlumniApp.server.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,24 +16,18 @@ import edu.kit.isco.KitAlumniApp.server.parser.HtmlParser;
 import edu.kit.isco.KitAlumniApp.server.parser.KitNewsParser;
 
 
-@Path("/getJSON")
+@Path("/service")
 public class JerseyRestService {
 	
 	@GET
-	@Path("/{type}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<News> produseJSON( @PathParam("type") String type ) {
-		switch (type) {
-		case "news":
-			
+	@Path("/news")
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	public List<News> produseJSON(  ) {
 			HtmlParser<News> kitNewsParser = new KitNewsParser();
 	        kitNewsParser.init();
 	        ArrayList<News> news = kitNewsParser.parseContent();
 			
 			return news;
-		default:
-			return null;
-		}
 		
 	}
 }
