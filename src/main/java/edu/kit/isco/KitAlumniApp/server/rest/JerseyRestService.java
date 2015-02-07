7f007c5932ccbf73571988a43d9464801e11a832
uafhf <uafhf@student.kit.edu>
2015-02-07 16:10:25 +0100

    Erste Implementierungsversion der Rest-Services

--- src/main/java/edu/kit/isco/KitAlumniApp/server/rest/JerseyRestService.java
+++ src/main/java/edu/kit/isco/KitAlumniApp/server/rest/JerseyRestService.java
@@ -1,10 +1,12 @@
 package edu.kit.isco.KitAlumniApp.server.rest;
 
-import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -18,8 +20,7 @@ import edu.kit.isco.KitAlumniApp.server.dataobject.DataAccessEvent;
 import edu.kit.isco.KitAlumniApp.server.dataobject.DataAccessJob;
 import edu.kit.isco.KitAlumniApp.server.dataobject.DataAccessNews;
 import edu.kit.isco.KitAlumniApp.server.dataobject.DataAccessUser;
-import edu.kit.isco.KitAlumniApp.server.parser.HtmlParser;
-import edu.kit.isco.KitAlumniApp.server.parser.KitNewsParser;
+import edu.kit.isco.KitAlumniApp.server.dbservices.DbHandlerService;
 
 
 @Path("/service")
@@ -28,36 +29,70 @@ public class JerseyRestService {
 	@GET
 	@Path("/news/latest")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-	public List<DataAccessNews> latestNews(@QueryParam("id") long id, @QueryParam("count") long count) {
-		return null;
+	public List<DataAccessNews> latestNewsById(@DefaultValue("-1") @QueryParam("id") long id) {
+		List<DataAccessNews> news = null;
+		if (id < 0) {
+			news = DbHandlerService.getAllNews();
+			if (news.size() > 30)
+				news = news.subList(news.size() - 30, news.size());
+		} else {
+			news = DbHandlerService.getLatestNews(id);
+		}
+		return news;
 	}
 	
 	@GET
 	@Path("/news/previous")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public List<DataAccessNews> previousNews(@QueryParam("id") long id, @QueryParam("count") long count) {
-		return null;
+		List<DataAccessNews> news = null;
+		if (id >= 0 && count > 0) {
+			news = DbHandlerService.getPreviousNews(id, count);
+		}
+		return news;
 	}
 	
 	@GET
 	@Path("/jobs/latest")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-	public List<DataAccessJob> latestJobs(@QueryParam("id") long id, @QueryParam("count") long count) {
-		return null;
+	public List<DataAccessJob> latestJobs(@DefaultValue("-1") @QueryParam("id") long id) {
+		List<DataAccessJob> jobs = null;
+		if (id < 0) {
+			jobs = DbHandlerService.getAllJobs();
+			if (jobs.size() > 30)
+				jobs = jobs.subList(jobs.size() - 30, jobs.size());
+		} else {
+			jobs = DbHandlerService.getLatestJobs(id);
+		}
+		return jobs;
 	}
 	
 	@GET
 	@Path("/jobs/previous")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public List<DataAccessJob> previousJobs(@QueryParam("id") long id, @QueryParam("count") long count) {
-		return null;
+		List<DataAccessJob> jobs = null;
+		if (id >= 0 && count > 0) {
+			jobs = DbHandlerService.getPreviousJobs(id, count);
+		}
+		return jobs;
 	}
 	
 	@GET
 	@Path("/events")
 	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public List<DataAccessEvent> nextEvents() {
-		return null;
+		Calendar date = Calendar.getInstance();
+		date.add(Calendar.MONTH, 1);
+		
+		List<DataAccessEvent> events = DbHandlerService.getAllEvents();
+		for (DataAccessEvent e : events) {
+			if (e.getDate().getTimeInMillis() > date.getTimeInMillis() &&
+					e.getDate().getTimeInMillis() < Calendar.getInstance().getTimeInMillis()) {
+				events.remove(e);
+			}
+		}
+		return events;
 	}
 	
 	
@@ -65,21 +100,25 @@ public class JerseyRestService {
 	@Path("/users/{user}")
 	@Consumes("application/json")
 	public void registerUser(@PathParam("user") DataAccessUser user) {
-		
+		DbHandlerService.saveUser(user);
 	}
 	
 	@PUT
 	@Path("users/{user}")
 	@Consumes("application/json")
 	public void updateUser(@PathParam("user") DataAccessUser user) {
-		
+		DataAccessUser prev = DbHandlerService.getUser(user.getClientId());
+		prev.setClientId(user.getClientId());
+		prev.setPassword(user.getPassword());
+		prev.setTags(user.getTags());
+		DbHandlerService.saveUser(prev);
 	}
 	
 	 @DELETE
 	 @Path("users/{user}")
 	 @Consumes("application/json")
 	 public void deleteUser(@PathParam("user") DataAccessUser user) {
-		 
+		 DbHandlerService.deleteUser(user.getClientId());
 	 }
 	
 }
