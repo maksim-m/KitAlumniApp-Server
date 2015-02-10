2eb88de2fb0605856b7845d0523d8ca21d5c95e9
Stelian Stoev <ugdhx@student.kit.edu>
2015-02-10 16:34:39 +0100

    Nur Multicast Nachrichten senden. Es stört nicht, wenn es nur ein Gerät in der Liste steht. Man sendet trozdem Multicast.

--- src/main/java/edu/kit/isco/KitAlumniApp/server/gcm/servlets/SendAllMessagesServlet.java
+++ src/main/java/edu/kit/isco/KitAlumniApp/server/gcm/servlets/SendAllMessagesServlet.java
@@ -30,8 +30,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.kit.isco.KitAlumniApp.server.gcm.ApiKeyInitializer;
+import edu.kit.isco.KitAlumniApp.server.gcm.Constants;
 import edu.kit.isco.KitAlumniApp.server.gcm.Datastore;
 import edu.kit.isco.KitAlumniApp.server.gcm.Message;
+import edu.kit.isco.KitAlumniApp.server.gcm.MulticastResult;
 import edu.kit.isco.KitAlumniApp.server.gcm.Result;
 import edu.kit.isco.KitAlumniApp.server.gcm.Sender;
 
@@ -77,20 +79,75 @@ public class SendAllMessagesServlet extends BaseServlet {
     } else {
       // NOTE: check below is for demonstration purposes; a real application
       // could always send a multicast, even for just one recipient
-      if (devices.size() == 1) {
-        // send a single message using plain post
-        String registrationId = devices.get(0);
-         Message message = new Message.Builder()
-        .collapseKey("job")
-        .timeToLive(3)
-        .delayWhileIdle(true)
-        .dryRun(true)
-        .addData("job","Awesome")
-        .build();
-        Result result = sender.send(message, registrationId, 5);
-        status = "Sent message to one device: " + result;
+    	// send a multicast message using JSON
+        // must split in chunks of 1000 devices (GCM limit)
+        int total = devices.size();
+        List<String> partialDevices = new ArrayList<String>(total);
+        int counter = 0;
+        int tasks = 0;
+        for (String device : devices) {
+          counter++;
+          partialDevices.add(device);
+          int partialSize = partialDevices.size();
+          if (partialSize == MULTICAST_SIZE || counter == total) {
+            asyncSend(partialDevices);
+            partialDevices.clear();
+            tasks++;
+          }
       }
+        status = "Asynchronously sending " + tasks + " multicast messages to " +
+                total + " devices";
     }
+    req.setAttribute(HomeServlet.ATTRIBUTE_STATUS, status.toString());
     getServletContext().getRequestDispatcher("/home").forward(req, resp);
   }
+  
+  private void asyncSend(List<String> partialDevices) {
+	    // make a copy
+	    final List<String> devices = new ArrayList<String>(partialDevices);
+	    threadPool.execute(new Runnable() {
+
+	      public void run() {
+	    	  Message message = new Message.Builder()
+	          .collapseKey("job")
+	          .timeToLive(3)
+	          .delayWhileIdle(true)
+	          .dryRun(true)
+	          .addData("job","Awesome")
+	          .build();
+	        MulticastResult multicastResult;
+	        try {
+	          multicastResult = sender.send(message, devices, 5);
+	        } catch (IOException e) {
+	          logger.log(Level.SEVERE, "Error posting messages", e);
+	          return;
+	        }
+	        List<Result> results = multicastResult.getResults();
+	        // analyze the results
+	        for (int i = 0; i < devices.size(); i++) {
+	          String regId = devices.get(i);
+	          Result result = results.get(i);
+	          String messageId = result.getMessageId();
+	          if (messageId != null) {
+	            logger.fine("Succesfully sent message to device: " + regId +
+	                "; messageId = " + messageId);
+	            String canonicalRegId = result.getCanonicalRegistrationId();
+	            if (canonicalRegId != null) {
+	              // same device has more than on registration id: update it
+	              logger.info("canonicalRegId " + canonicalRegId);
+	              Datastore.updateRegistration(regId, canonicalRegId);
+	            }
+	          } else {
+	            String error = result.getErrorCodeName();
+	            if (error.equals(Constants.ERROR_NOT_REGISTERED)) {
+	              // application has been removed from device - unregister it
+	              logger.info("Unregistered device: " + regId);
+	              Datastore.unregister(regId);
+	            } else {
+	              logger.severe("Error sending message to " + regId + ": " + error);
+	            }
+	          }
+	        }
+	      }});
+	  }
 }
